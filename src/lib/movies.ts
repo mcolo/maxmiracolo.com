22442c3c5dabe40e98032ba1787182b99244ea58
Max Miracolo <max.miracolo@gmail.com>
2025-09-08 16:58:21 -0400

    halloween page

---
+++ src/lib/movies.ts
@@ -0,0 +1,571 @@
+export const movies = [
+  {
+    "showtime_date": "September 19th",
+    "adult": false,
+    "backdrop_path": "/l0JgqFNxeMb1OoHq1CzdYC3Y5J8.jpg",
+    "belongs_to_collection": {
+      "id": 98580,
+      "name": "Candyman Collection",
+      "poster_path": "/4X8k67rccwSldxjFPxxP7x3LjJv.jpg",
+      "backdrop_path": "/87UMdcpugFmgVz2VgEopExuckMc.jpg"
+    },
+    "budget": 6000000,
+    "genres": [
+      {
+        "id": 18,
+        "name": "Drama"
+      },
+      {
+        "id": 27,
+        "name": "Horror"
+      },
+      {
+        "id": 53,
+        "name": "Thriller"
+      }
+    ],
+    "homepage": "",
+    "id": 9529,
+    "imdb_id": "tt0103919",
+    "origin_country": [
+      "US"
+    ],
+    "original_language": "en",
+    "original_title": "Candyman",
+    "overview": "The Candyman, a murderous soul with a hook for a hand, is accidentally summoned to reality by a skeptic grad student researching the monster's myth.",
+    "popularity": 2.8713,
+    "poster_path": "/jQtgkgDZE7egMq532sOt83DnT83.jpg",
+    "production_companies": [
+      {
+        "id": 278,
+        "logo_path": "/r3Aya3Sy7f24t6Waaug3mQn6ew7.png",
+        "name": "Propaganda Films",
+        "origin_country": "US"
+      },
+      {
+        "id": 1382,
+        "logo_path": "/8b5XGJ8YhZoEo9LgFP8KRQWHjYL.png",
+        "name": "PolyGram Filmed Entertainment",
+        "origin_country": "US"
+      },
+      {
+        "id": 159993,
+        "logo_path": null,
+        "name": "Candyman Films",
+        "origin_country": ""
+      },
+      {
+        "id": 559,
+        "logo_path": "/eC0bWHVjnjUducyA6YFoEFqnPMC.png",
+        "name": "TriStar Pictures",
+        "origin_country": "US"
+      }
+    ],
+    "production_countries": [
+      {
+        "iso_3166_1": "US",
+        "name": "United States of America"
+      }
+    ],
+    "release_date": "1992-10-16",
+    "revenue": 25792310,
+    "runtime": 100,
+    "spoken_languages": [
+      {
+        "english_name": "English",
+        "iso_639_1": "en",
+        "name": "English"
+      }
+    ],
+    "status": "Released",
+    "tagline": "The pain will be exquisite.",
+    "title": "Candyman",
+    "video": false,
+    "vote_average": 6.6,
+    "vote_count": 1684
+  },
+  {
+    "showtime_date": "September 26th",
+    "adult": false,
+    "backdrop_path": "/kSeU4uk19t82k6Cao7hc6ktDYhF.jpg",
+    "belongs_to_collection": null,
+    "budget": 4500000,
+    "genres": [
+      {
+        "id": 27,
+        "name": "Horror"
+      },
+      {
+        "id": 53,
+        "name": "Thriller"
+      }
+    ],
+    "homepage": "https://www.barbariantickets.ca/",
+    "id": 913290,
+    "imdb_id": "tt15791034",
+    "origin_country": [
+      "US"
+    ],
+    "original_language": "en",
+    "original_title": "Barbarian",
+    "overview": "In town for a job interview, a young woman arrives at her Airbnb late at night only to find that it has been mistakenly double-booked and a strange man is already staying there. Against her better judgement, she decides to stay the night anyway.",
+    "popularity": 7.6057,
+    "poster_path": "/idT5mnqPcJgSkvpDX7pJffBzdVH.jpg",
+    "production_companies": [
+      {
+        "id": 18609,
+        "logo_path": "/pxtYCfl1DWzvizqZcYwE4NefOOF.png",
+        "name": "BoulderLight Pictures",
+        "origin_country": "US"
+      },
+      {
+        "id": 829,
+        "logo_path": "/iJo9zP5QCKyBsmKSOcvXd6UDNih.png",
+        "name": "Vertigo Entertainment",
+        "origin_country": "US"
+      },
+      {
+        "id": 508,
+        "logo_path": "/4sGWXoboEkWPphI6es6rTmqkCBh.png",
+        "name": "Regency Enterprises",
+        "origin_country": "US"
+      },
+      {
+        "id": 106754,
+        "logo_path": "/1H4YyGx74OTzPtPIcuF3iAEjzz6.png",
+        "name": "Hammerstone Studios",
+        "origin_country": "US"
+      },
+      {
+        "id": 111475,
+        "logo_path": "/vNz6AhnDHvpiSHSc3OzHyDawr0Z.png",
+        "name": "Almost Never Films",
+        "origin_country": "US"
+      }
+    ],
+    "production_countries": [
+      {
+        "iso_3166_1": "US",
+        "name": "United States of America"
+      }
+    ],
+    "release_date": "2022-09-08",
+    "revenue": 45400000,
+    "runtime": 102,
+    "spoken_languages": [
+      {
+        "english_name": "English",
+        "iso_639_1": "en",
+        "name": "English"
+      },
+      {
+        "english_name": "Spanish",
+        "iso_639_1": "es",
+        "name": "Español"
+      }
+    ],
+    "status": "Released",
+    "tagline": "Some stay for a night. Some stay for a week. Some never leave.",
+    "title": "Barbarian",
+    "video": false,
+    "vote_average": 6.833,
+    "vote_count": 2836
+  },
+  {
+    "showtime_date": "October 3rd",
+    "adult": false,
+    "backdrop_path": "/4anqXDsl5iZ26qeTRX3yZWylMgY.jpg",
+    "belongs_to_collection": {
+      "id": 430201,
+      "name": "Demons Collection",
+      "poster_path": "/vv9EnK2hji6AFes0WVsqh9KRvKR.jpg",
+      "backdrop_path": "/uiVuCBjOpi5esYmdT50WnaibB9p.jpg"
+    },
+    "budget": 1800000,
+    "genres": [
+      {
+        "id": 27,
+        "name": "Horror"
+      }
+    ],
+    "homepage": "",
+    "id": 14029,
+    "imdb_id": "tt0089013",
+    "origin_country": [
+      "IT"
+    ],
+    "original_language": "it",
+    "original_title": "Dèmoni",
+    "overview": "A group of people are trapped in a West Berlin movie theater infested with ravenous demons who proceed to kill and possess the humans one-by-one, thereby multiplying their numbers.",
+    "popularity": 3.3941,
+    "poster_path": "/ym0mtcSt6i45NIcAoSBQhop0wSP.jpg",
+    "production_companies": [
+      {
+        "id": 4746,
+        "logo_path": null,
+        "name": "DACFILM Rome",
+        "origin_country": "IT"
+      }
+    ],
+    "production_countries": [
+      {
+        "iso_3166_1": "IT",
+        "name": "Italy"
+      }
+    ],
+    "release_date": "1985-10-04",
+    "revenue": 0,
+    "runtime": 89,
+    "spoken_languages": [
+      {
+        "english_name": "English",
+        "iso_639_1": "en",
+        "name": "English"
+      },
+      {
+        "english_name": "Italian",
+        "iso_639_1": "it",
+        "name": "Italiano"
+      }
+    ],
+    "status": "Released",
+    "tagline": "They will make cemeteries their cathedrals and the cities will be your tombs.",
+    "title": "Demons",
+    "video": false,
+    "vote_average": 6.61,
+    "vote_count": 596
+  },
+  {
+    "showtime_date": "October 10th",
+    "adult": false,
+    "backdrop_path": "/bizhlTVjifYQUu4Xrdt7m3TYr7d.jpg",
+    "belongs_to_collection": null,
+    "budget": 10000000,
+    "genres": [
+      {
+        "id": 80,
+        "name": "Crime"
+      },
+      {
+        "id": 27,
+        "name": "Horror"
+      },
+      {
+        "id": 53,
+        "name": "Thriller"
+      }
+    ],
+    "homepage": "https://neonrated.com/films/longlegs",
+    "id": 1226578,
+    "imdb_id": "tt23468450",
+    "origin_country": [
+      "US"
+    ],
+    "original_language": "en",
+    "original_title": "Longlegs",
+    "overview": "FBI Agent Lee Harker is a gifted new recruit assigned to the unsolved case of an elusive serial killer. As the case takes complex turns, unearthing evidence of the occult, Harker discovers a personal connection to the merciless killer and must race against time to stop him before he claims the lives of another innocent family.",
+    "popularity": 11.0251,
+    "poster_path": "/1EwNyiiNFd863H4e8nWEzutnZD7.jpg",
+    "production_companies": [
+      {
+        "id": 184518,
+        "logo_path": "/lOouI8ibBzhLI4qvku74FPUVT8t.png",
+        "name": "C2 Motion Picture Group",
+        "origin_country": "US"
+      },
+      {
+        "id": 831,
+        "logo_path": "/vLENXiYTyITnMDrTKabUXhgTKX2.png",
+        "name": "Saturn Films",
+        "origin_country": "US"
+      },
+      {
+        "id": 85465,
+        "logo_path": "/oxx1bmXDQxeBsvYZmCxMtfJ5wa3.png",
+        "name": "Oddfellows Entertainment",
+        "origin_country": "CA"
+      },
+      {
+        "id": 232289,
+        "logo_path": "/bV35YIaGQvRtjsD2r8KdlvYDKrp.png",
+        "name": "Traffic.",
+        "origin_country": "US"
+      },
+      {
+        "id": 167684,
+        "logo_path": "/kzQwU2H6woBFti9cqjr9BrVXrst.png",
+        "name": "Range Media Partners",
+        "origin_country": "US"
+      }
+    ],
+    "production_countries": [
+      {
+        "iso_3166_1": "CA",
+        "name": "Canada"
+      },
+      {
+        "iso_3166_1": "US",
+        "name": "United States of America"
+      }
+    ],
+    "release_date": "2024-07-10",
+    "revenue": 127961936,
+    "runtime": 101,
+    "spoken_languages": [
+      {
+        "english_name": "English",
+        "iso_639_1": "en",
+        "name": "English"
+      }
+    ],
+    "status": "Released",
+    "tagline": "Say your prayers.",
+    "title": "Longlegs",
+    "video": false,
+    "vote_average": 6.6,
+    "vote_count": 2225
+  },
+  {
+    "showtime_date": "October 17th",
+    "adult": false,
+    "backdrop_path": "/edghRD7vw1XgWEpMLpGt1VSGB5w.jpg",
+    "belongs_to_collection": {
+      "id": 10455,
+      "name": "Child's Play Collection",
+      "poster_path": "/fLWDbZziq47lcN28Q1ILnIMLHEC.jpg",
+      "backdrop_path": "/aWLxBxRtQP5a6oKkXqGkGYEPmNX.jpg"
+    },
+    "budget": 9000000,
+    "genres": [
+      {
+        "id": 27,
+        "name": "Horror"
+      }
+    ],
+    "homepage": "",
+    "id": 10585,
+    "imdb_id": "tt0094862",
+    "origin_country": [
+      "US"
+    ],
+    "original_language": "en",
+    "original_title": "Child's Play",
+    "overview": "An innocent-looking doll is inhabited by the soul of a serial killer who refuses to die.",
+    "popularity": 6.2399,
+    "poster_path": "/wvpgvcWNkF2HLuTEMIM7K83MvZ.jpg",
+    "production_companies": [
+      {
+        "id": 60,
+        "logo_path": "/4YldLvCWQL9VIAHQ2Fu3Ffkh9Si.png",
+        "name": "United Artists",
+        "origin_country": "US"
+      },
+      {
+        "id": 11050,
+        "logo_path": null,
+        "name": "David Kirschner Productions",
+        "origin_country": "US"
+      },
+      {
+        "id": 21,
+        "logo_path": "/usUnaYV6hQnlVAXP6r4HwrlLFPG.png",
+        "name": "Metro-Goldwyn-Mayer",
+        "origin_country": "US"
+      }
+    ],
+    "production_countries": [
+      {
+        "iso_3166_1": "US",
+        "name": "United States of America"
+      }
+    ],
+    "release_date": "1988-11-09",
+    "revenue": 44196684,
+    "runtime": 87,
+    "spoken_languages": [
+      {
+        "english_name": "English",
+        "iso_639_1": "en",
+        "name": "English"
+      }
+    ],
+    "status": "Released",
+    "tagline": "You'll wish it was only make-believe.",
+    "title": "Child's Play",
+    "video": false,
+    "vote_average": 6.727,
+    "vote_count": 3140
+  },
+  {
+    "showtime_date": "October 24th",
+    "adult": false,
+    "backdrop_path": "/tweDJNQzBGgsWVF5MC8JhSAk07p.jpg",
+    "belongs_to_collection": null,
+    "budget": 7000000,
+    "genres": [
+      {
+        "id": 53,
+        "name": "Thriller"
+      },
+      {
+        "id": 878,
+        "name": "Science Fiction"
+      },
+      {
+        "id": 27,
+        "name": "Horror"
+      }
+    ],
+    "homepage": "https://www.theinvisiblemanmovie.com/",
+    "id": 570670,
+    "imdb_id": "tt1051906",
+    "origin_country": [
+      "AU",
+      "US"
+    ],
+    "original_language": "en",
+    "original_title": "The Invisible Man",
+    "overview": "When Cecilia's abusive ex takes his own life and leaves her his fortune, she suspects his death was a hoax. As a series of coincidences turn lethal, Cecilia works to prove that she is being hunted by someone nobody can see.",
+    "popularity": 7.5156,
+    "poster_path": "/5EufsDwXdY2CVttYOk2WtYhgKpa.jpg",
+    "production_companies": [
+      {
+        "id": 3172,
+        "logo_path": "/rzKluDcRkIwHZK2pHsiT667A2Kw.png",
+        "name": "Blumhouse Productions",
+        "origin_country": "US"
+      },
+      {
+        "id": 6748,
+        "logo_path": "/74qmkZFGFMbCsIKo2Dk62QVsUKf.png",
+        "name": "Goalpost Pictures",
+        "origin_country": "AU"
+      },
+      {
+        "id": 107787,
+        "logo_path": null,
+        "name": "Nervous Tick Productions",
+        "origin_country": "AU"
+      },
+      {
+        "id": 33,
+        "logo_path": "/8lvHyhjr8oUKOOy2dKXoALWKdp0.png",
+        "name": "Universal Pictures",
+        "origin_country": "US"
+      }
+    ],
+    "production_countries": [
+      {
+        "iso_3166_1": "AU",
+        "name": "Australia"
+      },
+      {
+        "iso_3166_1": "US",
+        "name": "United States of America"
+      }
+    ],
+    "release_date": "2020-02-26",
+    "revenue": 144492724,
+    "runtime": 124,
+    "spoken_languages": [
+      {
+        "english_name": "English",
+        "iso_639_1": "en",
+        "name": "English"
+      }
+    ],
+    "status": "Released",
+    "tagline": "What You Can't See Can Hurt You",
+    "title": "The Invisible Man",
+    "video": false,
+    "vote_average": 7.1,
+    "vote_count": 5967
+  },
+  {
+    "showtime_date": "October 31st",
+    "adult": false,
+    "backdrop_path": "/g9ROrZey0JYp7kf8DoAyZtKnObj.jpg",
+    "belongs_to_collection": {
+      "id": 8581,
+      "name": "A Nightmare on Elm Street Collection",
+      "poster_path": "/6ihRddI1Pg4wHFhkZwI9mERRrMd.jpg",
+      "backdrop_path": "/7UcPLgNneoemYmZcyUo5r3nwBbE.jpg"
+    },
+    "budget": 1800000,
+    "genres": [
+      {
+        "id": 27,
+        "name": "Horror"
+      }
+    ],
+    "homepage": "",
+    "id": 377,
+    "imdb_id": "tt0087800",
+    "origin_country": [
+      "US"
+    ],
+    "original_language": "en",
+    "original_title": "A Nightmare on Elm Street",
+    "overview": "Teenagers in a small town are dropping like flies, apparently in the grip of mass hysteria causing their suicides. A cop's daughter, Nancy Thompson, traces the cause to child molester Fred Krueger, who was burned alive by angry parents many years before. Krueger has now come back in the dreams of his killers' children, claiming their lives as his revenge. Nancy and her boyfriend, Glen, must devise a plan to lure the monster out of the realm of nightmares and into the real world...",
+    "popularity": 8.3253,
+    "poster_path": "/wGTpGGRMZmyFCcrY2YoxVTIBlli.jpg",
+    "production_companies": [
+      {
+        "id": 12,
+        "logo_path": "/2ycs64eqV5rqKYHyQK0GVoKGvfX.png",
+        "name": "New Line Cinema",
+        "origin_country": "US"
+      },
+      {
+        "id": 1531,
+        "logo_path": null,
+        "name": "Smart Egg Pictures",
+        "origin_country": "GB"
+      },
+      {
+        "id": 18329,
+        "logo_path": "/6WnB9RtZxmnUYIbBwWjnuORIcGg.png",
+        "name": "Media Home Entertainment",
+        "origin_country": ""
+      },
+      {
+        "id": 4764,
+        "logo_path": null,
+        "name": "Cinema 84",
+        "origin_country": "US"
+      },
+      {
+        "id": 176294,
+        "logo_path": null,
+        "name": "The Elm Street Venture",
+        "origin_country": ""
+      }
+    ],
+    "production_countries": [
+      {
+        "iso_3166_1": "US",
+        "name": "United States of America"
+      },
+      {
+        "iso_3166_1": "GB",
+        "name": "United Kingdom"
+      }
+    ],
+    "release_date": "1984-11-09",
+    "revenue": 57004513,
+    "runtime": 91,
+    "spoken_languages": [
+      {
+        "english_name": "English",
+        "iso_639_1": "en",
+        "name": "English"
+      }
+    ],
+    "status": "Released",
+    "tagline": "If Nancy doesn't wake up screaming she won't wake up at all.",
+    "title": "A Nightmare on Elm Street",
+    "video": false,
+    "vote_average": 7.341,
+    "vote_count": 5412
+  }
+]
